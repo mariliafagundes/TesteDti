dff25e681a370b837bcc465eccc7870c2462278e
mariliafagundes <156796776+mariliafagundes@users.noreply.github.com>
2024-03-31 17:11:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def test_login_credenciais_validas():
     password_input.send_keys("senha123")
     login_button = driver.find_element(By.XPATH,'//*[@id="SubmitLogin"]/span')
     login_button.click()
-    time.sleep(2)  # Espera para garantir que a página seja carregada
+    time.sleep(3)  # Espera para garantir que a página seja carregada
     # Então o usuário deve ser redirecionado para a página da sua conta
     try:
         elemento_pagina_conta = driver.find_element(By.CLASS_NAME, 'info-account')
@@ -42,7 +42,7 @@ def test_login_email_invalido():
     password_input.send_keys("examplepassword")
     login_button = driver.find_element(By.XPATH,'//*[@id="SubmitLogin"]/span')
     login_button.click()
-    time.sleep(2)  # Espera para garantir que a página seja carregada
+    time.sleep(3)  # Espera para garantir que a página seja carregada
     get_source = driver.page_source
     search_text = "Invalid email address."
     # Então o usuário deve receber uma mensagem de email inválido.
@@ -65,7 +65,7 @@ def test_login_senha_incorreta():
     password_input.send_keys("123")
     login_button = driver.find_element(By.XPATH, '//*[@id="SubmitLogin"]/span')
     login_button.click()
-    time.sleep(2)  # Espera para garantir que a página seja carregada
+    time.sleep(3)  # Espera para garantir que a página seja carregada
 
     # Então o usuário deve receber uma mensagem de senha inválida.
     get_source = driver.page_source
